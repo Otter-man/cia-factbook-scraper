092d96ace936f32c6860a98f639e5f85da5ab5cf
Otter-man <tereschenko.danila@gmail.com>
2020-06-29 17:59:00 +0700

    changed download method for pdfs from wget to requests-html get.

--- scripts/One-page_summaries_downloader.py
+++ scripts/One-page_summaries_downloader.py
@@ -1,5 +1,4 @@
 from requests_html import HTML, HTMLSession
-import wget
 import os
 
 
@@ -14,14 +13,12 @@ def pdf_link_scraper():
     session = HTMLSession()
     source = session.get(cia_page).html  # downloading link as html
 
-    country_data = source.find(
-        "div.country-name"
-    )  # finding all blocks containing country name and link to onepage summary
+    # finding all blocks containing country name and link to onepage summary
+    country_data = source.find("div.country-name")
 
     for country in country_data:
-        links[country.text] = list(country.absolute_links)[
-            0
-        ]  # add coyntry name as a key and link to pdf summary as a value to dictionary
+        # add contry name as a key and link to pdf summary as a value to dictionary for each country
+        links[country.text] = list(country.absolute_links)[0]
 
     return links  # returning resulting dictionary
 
@@ -34,17 +31,16 @@ def download_pdf(links):
     if not os.path.exists("pdf"):  # check if folder "pdf" exists
         os.mkdir("pdf")  # creating the folder if not
 
-    for (
-        country,
-        link,
-    ) in links.items():  # we take dictionary resulting from the previous function
-        file_name = (
-            country + ".pdf"
-        )  # create filename for a pdf using keys from dictionary
+    # we take dictionary resulting from the previous function
+    for country, link in links.items():
+
+        # create filename for a pdf using keys from dictionary
+        file_name = country + ".pdf"
         file_path = os.path.join("pdf", file_name)  # create path for downloading pdf
-        wget.download(
-            link, file_path
-        )  # download pdf using links from values in dictionary
+
+        with open(file_path, "wb") as pdf_file:  # downloading pdf file
+            r = HTMLSession().get(link)
+            pdf_file.write(r.content)
 
 
 links = pdf_link_scraper()
