bd53823a56f18b906e309d4dd415c0691f10c1cb
Otter-man <tereschenko.danila@gmail.com>
2020-06-30 20:52:22 +0700

    changed pdf scraping, added scraping text to list

--- scripts/pdf_to_text_script.py
+++ scripts/pdf_to_text_script.py
@@ -5,17 +5,21 @@ import os
 def pdf_scraper():
     path_to_pdf = "pdf"
     path_to_text = "pdf_text"
+    path_to_listed_text = "list_text"
 
     if not os.path.exists(path_to_text):
         os.mkdir(path_to_text)
+
+    if not os.path.exists(path_to_listed_text):
+        os.mkdir(path_to_listed_text)
 
     # making parameters for PDFminer for this specific PDFs
     la_params = layout.LAParams(
         line_overlap=0.4,
         char_margin=2.0,
-        line_margin=0.9,
+        line_margin=1.0,
         word_margin=0.1,
-        boxes_flow=0.5,
+        boxes_flow=0.3,
         detect_vertical=False,
         all_texts=False,
     )
@@ -24,9 +28,17 @@ def pdf_scraper():
         filepath = os.path.join(path_to_pdf, name)
 
         with open(os.path.join(path_to_text, f"{name[:-3]}txt"), "w") as text_file:
+            with open(
+                os.path.join(path_to_listed_text, f"{name[:-3]}list.txt"), "w"
+            ) as list_file:
 
-            text = high_level.extract_text(filepath, laparams=la_params)
-            text_file.write(text)
+                text = high_level.extract_text(filepath, laparams=la_params)
+                text_file.write(text)
+
+                pdf_as_list = [
+                    item for item in text.split("\n") if item != "" and item != "\x0c"
+                ]
+                list_file.write(str(pdf_as_list))
 
     print("Finished scraping text")
 
