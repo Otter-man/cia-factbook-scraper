a81fd9c0687ed0f4699ee5d87f151608beab3560
Otter-man <tereschenko.danila@gmail.com>
2020-07-18 20:07:56 +0700

    added some comments, refactored code alittle

--- scripts/pdf_to_text_script.py
+++ scripts/pdf_to_text_script.py
@@ -3,35 +3,38 @@ import os
 import csv
 import ast
 
-###here be functions
-def get_index(fields, text, country_id):
+
+def get_index(fields, text_splited, country_id):
     """this function builds dict of indexes
     for every country with indexes of elements,
-    that represent the rows in the csv
-    returns a dict of indexes"""
+    that represent the columns in the DB-table and
+    returns a dict of indexes.
+    It takes list of fields for indexing, text, splited by whitespaces,
+    so every word is an element of a list and country_ID 
+    for handling some exceptions in naming"""
 
     index_dict = {}
 
-    # deleting unnecesary headers in text
-    del text[text.index("GOVERNMENT")]
-    del text[text.index("GEOGRAPHY")]
+    # deleting unnecesary headers in text_splited
+    del text_splited[text_splited.index("GOVERNMENT")]
+    del text_splited[text_splited.index("GEOGRAPHY")]
 
     pep_soc = [
         (i, i + 3)
-        for i in range(len(text))
-        if text[i : i + 3] == ["PEOPLE", "&", "SOCIETY"]
+        for i in range(len(text_splited))
+        if text_splited[i : i + 3] == ["PEOPLE", "&", "SOCIETY"]
     ]
-    del text[pep_soc[0][0] : pep_soc[0][1]]
+    del text_splited[pep_soc[0][0] : pep_soc[0][1]]
 
-    if "'Economic', 'Overview'" in str(text):
-        del text[text.index("ECONOMY")]
+    if "'Economic', 'Overview'" in str(text_splited):
+        del text_splited[text_splited.index("ECONOMY")]
 
     # geting index for each field
     for item in fields:
 
         item_list = item.split()
 
-        # exception for some pages that contain errors in naming
+        # this part handles some exception for some pages that contain errors in naming
         if country_id == "GERMANY" and item == "GDP (Purchasing Power Parity)":
             item_list = "GDP Purchasing Power Parity)".split()
         elif country_id == "MOLDOVA" and item == "US Ambassador":
@@ -45,12 +48,21 @@ def get_index(fields, text, country_id):
         ) and item == "Economic Overview":
             item_list = ["ECONOMY"]
 
+        # this part finds build indexes for every field name
+        # every index consists of start index (index of the first word of field name)
+        # and end index (index of the last word of field name) inside a tuple inside a list
+        # in most cases there is only one tuple for each field name.
         index = [
             (i, i + len(item_list))
-            for i in range(len(text))
-            if text[i : i + len(item_list)] == item_list
+            for i in range(len(text_splited))
+            if text_splited[i : i + len(item_list)] == item_list
         ]
 
+        # but some times there are more than one occurence of the field name
+        # in text and so there are more than one index tuple.
+        # With field "US ambassador" we always use second occurence of fieldname
+        # (second tuple in list of tuples)
+        # with other - we always use first.
         if len(index) > 1 and item == "US Ambassador":
             index = [i for i in index[1]]
         elif len(index) > 0:
@@ -63,11 +75,12 @@ def get_index(fields, text, country_id):
 
 def pdf_scraper(path_to_pdf):
     """this functiion takes path to pdf folder then scrapes 
-    each pdf for text, then breaks text down and sorts it by fields.
+    each pdf for text, then splits text down as list, where every word is an element.
+    Then it slices text by field names.
     Returns a dict object which consists of country names as a key
     and nested dictionaris as values.
-    Nested dictionaries use fields for keys.
-    Return can be used to make a json file or csv file.
+    Nested dictionaries use fields for keys, and use content of the field as a value.
+    Return object can be used to make a csv file or fill a table in DB.
     """
 
     # making parameters for PDFminer for this specific PDFs
@@ -112,21 +125,21 @@ def pdf_scraper(path_to_pdf):
     for name in os.listdir(path_to_pdf):
         filepath = os.path.join(path_to_pdf, name)
 
+        # scraping unformated text using pdfminer.six
         text = high_level.extract_text(filepath, laparams=la_params)
 
+        # here we extract country name from text
         text = text.split("\n", 1)
-
-        country = text.pop(0)
-        if country == "SAO TOMEAND PRINCIPE":
-            country = "SAO TOME AND PRINCIPE"
-
-        text = text[0].split()
-        text = [country] + text
-
         country_id = text.pop(0)
+        if country_id == "SAO TOMEAND PRINCIPE":
+            country_id = "SAO TOME AND PRINCIPE"
+        # we split text by whitespaces and end-line characters, so text is presented as a list
+        # where every word and set of any characters such as numbers has index
+        text = text[0].split()
+
         nested_dict[country_id] = {}
 
-        # searching for the date of last update for PDF
+        # searching for the date of last update for PDF and removing it from text
         last_update_index = [
             (i, i + 2) for i in range(len(text)) if text[i : i + 2] == ["as", "of"]
         ]
@@ -140,36 +153,41 @@ def pdf_scraper(path_to_pdf):
         nested_dict[country_id]["last_update"] = last_update
 
         # We index text with get_index function
-        # then based on index we slice text scraped from PDF
-        # into nested dictionary which we use later for making csv
-
         index_dict = get_index(fields, text, country_id)
 
-        for item in fields[::-1]:
+        # we work with fieldnames in reverse order, since we need to parse
+        # text from end to begining
+        for fieldname in fields[::-1]:
 
-            nested_dict[country_id][item] = []
+            # this handles some expections for Sudan and Chad, where fields
+            # Chief of State and Head of Government are joined
+            if index_dict[fieldname] == [] and fieldname == "Chief of State":
+                nested_dict[country_id][fieldname] = " ".join(text[5:])
 
-            if index_dict[item] == [] and item == "Chief of State":
-                nested_dict[country_id][item] = " ".join(text[5:])
+            # some countries don't have some fields, like Literacy
+            # so with such countries we set value for this fields to NULL
+            elif index_dict[fieldname] == []:
+                nested_dict[country_id][fieldname] = "NULL"
 
-            elif index_dict[item] == []:
-                nested_dict[country_id][item] = "NULL"
+            # This is main part, that works with most of the text.
+            # It starts with the end of text and finds the last field
+            # based on index that we got from get_index function
+            # It creates fieldname as a key, and content of text as a value
+            # then it deletes fieldname and content from the splited text we work on.
+            # So one by one we delete each field from end to begining.
 
             else:
-                starting_index = index_dict[item][0]
-                nested_dict[country_id][item] = " ".join(
-                    text[starting_index + len(item.split()) :]
-                )
-                del text[starting_index:]
+                start_field = index_dict[fieldname][0]
+                start_content = index_dict[fieldname][1]
 
-    nested_dict = str(nested_dict).replace("NA", "NULL")
+                nested_dict[country_id][fieldname] = " ".join(text[start_content:])
+                del text[start_field:]
+
+    # some coutries have field content NA or N/A, we replace them with NULL for consistency
+    nested_dict = str(nested_dict).replace("'NA'", "'NULL'")
     nested_dict = nested_dict.replace("N/A", "NULL")
+
     nested_dict = ast.literal_eval(nested_dict)
 
     print("Finished scraping text")
     return nested_dict
-
-
-# country_dict = pdf_scraper("pdf")
-# with open("dict.txt", "w") as f:
-#     f.write(str(country_dict))
