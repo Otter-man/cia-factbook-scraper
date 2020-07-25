bf87d4cf4bb8df53bf24aec094a2a20b77d843d4
Otter-man <tereschenko.danila@gmail.com>
2020-07-25 22:29:59 +0700

    changed docstring for create_index function

--- scripts/pdf_to_text_script.py
+++ scripts/pdf_to_text_script.py
@@ -1,33 +1,46 @@
-from pdfminer import high_level, layout
+"""Takes PDF, converts them to text and scrapes data from it."""
 import os
-import csv
 import ast
+from pdfminer import high_level, layout
 
 
-def get_index(fields, text_splited, country_id):
-    """this function builds dict of indexes
-    for every country with indexes of elements,
-    that represent the columns in the DB-table and
-    returns a dict of indexes.
-    It takes list of fields for indexing, text, splited by whitespaces,
-    so every word is an element of a list and country_ID 
-    for handling some exceptions in naming"""
+def create_index(fields, text_split, country_id):
+    """Builds index dictionary with index for every field.
+
+    Args:
+        fields (list of str): list of field names.
+        text_split (list of str): text split to list, where every word
+            is an element of list.
+        country_id (str): name of the country, used to handle minor
+            errors in text while indexing specific countries.
+
+    Returns:
+        dict: dictionary, with field name as a key and tuple as a value.
+        Each tuple contains two int. First int - index of a first word of
+        a field name in text as list, second int is an index +1 of a last
+        word of a field name in text as list.
+
+    Examples:
+        print(create_index(["Climate", "Natural Resources"], ["Climate",
+        "and", "Natural", "Resources"], "Russia"))
+        {"Climate":(0,1), "Natural Resources":(2,4)}  
+    """
 
     index_dict = {}
 
-    # deleting unnecesary headers in text_splited
-    del text_splited[text_splited.index("GOVERNMENT")]
-    del text_splited[text_splited.index("GEOGRAPHY")]
+    # deleting unnecesary headers in text_split
+    del text_split[text_split.index("GOVERNMENT")]
+    del text_split[text_split.index("GEOGRAPHY")]
 
     pep_soc = [
         (i, i + 3)
-        for i in range(len(text_splited))
-        if text_splited[i : i + 3] == ["PEOPLE", "&", "SOCIETY"]
+        for i in range(len(text_split))
+        if text_split[i: i + 3] == ["PEOPLE", "&", "SOCIETY"]
     ]
-    del text_splited[pep_soc[0][0] : pep_soc[0][1]]
+    del text_split[pep_soc[0][0]: pep_soc[0][1]]
 
-    if "'Economic', 'Overview'" in str(text_splited):
-        del text_splited[text_splited.index("ECONOMY")]
+    if "'Economic', 'Overview'" in str(text_split):
+        del text_split[text_split.index("ECONOMY")]
 
     # geting index for each field
     for item in fields:
@@ -54,8 +67,8 @@ def get_index(fields, text_splited, country_id):
         # in most cases there is only one tuple for each field name.
         index = [
             (i, i + len(item_list))
-            for i in range(len(text_splited))
-            if text_splited[i : i + len(item_list)] == item_list
+            for i in range(len(text_split))
+            if text_split[i: i + len(item_list)] == item_list
         ]
 
         # but some times there are more than one occurence of the field name
@@ -74,7 +87,7 @@ def get_index(fields, text_splited, country_id):
 
 
 def pdf_scraper(path_to_pdf):
-    """this functiion takes path to pdf folder then scrapes 
+    """this functiion takes path to pdf folder then scrapes
     each pdf for text, then splits text down as list, where every word is an element.
     Then it slices text by field names.
     Returns a dict object which consists of country names as a key
@@ -141,26 +154,26 @@ def pdf_scraper(path_to_pdf):
 
         # searching for the date of last update for PDF and removing it from text
         last_update_index = [
-            (i, i + 2) for i in range(len(text)) if text[i : i + 2] == ["as", "of"]
+            (i, i + 2) for i in range(len(text)) if text[i: i + 2] == ["as", "of"]
         ]
 
         last_update = " ".join(
-            text[last_update_index[0][1] : last_update_index[0][1] + 2]
+            text[last_update_index[0][1]: last_update_index[0][1] + 2]
         )
         last_update = last_update.strip()
-        del text[last_update_index[0][0] : last_update_index[0][0] + 4]
+        del text[last_update_index[0][0]: last_update_index[0][0] + 4]
 
         nested_dict[country_id]["last_update"] = last_update
 
         # We index text with get_index function
-        index_dict = get_index(fields, text, country_id)
+        index_dict = create_index(fields, text, country_id)
 
         # we work with fieldnames in reverse order, since we need to parse
         # text from end to begining
         for fieldname in fields[::-1]:
 
-            # this handles some expections for Sudan and Chad, where fields
-            # Chief of State and Head of Government are joined
+            # this handles some expections for Sudan and Chad, where
+            # fields Chief of State and Head of Government are joined
             if index_dict[fieldname] == [] and fieldname == "Chief of State":
                 nested_dict[country_id][fieldname] = " ".join(text[5:])
 
@@ -180,7 +193,8 @@ def pdf_scraper(path_to_pdf):
                 start_field = index_dict[fieldname][0]
                 start_content = index_dict[fieldname][1]
 
-                nested_dict[country_id][fieldname] = " ".join(text[start_content:])
+                nested_dict[country_id][fieldname] = " ".join(
+                    text[start_content:])
                 del text[start_field:]
 
     # some coutries have field content NA or N/A, we replace them with NULL for consistency
