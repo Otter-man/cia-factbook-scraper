19fdab7b498660cf3f65a67297ab8ead4783223d
Otter-man <tereschenko.danila@gmail.com>
2020-07-27 22:09:47 +0700

    changed classes a little

--- scripts/class_module.py
+++ scripts/class_module.py
@@ -1,10 +1,10 @@
 """This module contains all classes used in the project."""
 
 
-class Country:
-    '''Class for storing data for each country in readable form.'''
+class CountryGeneral:
+    '''Class with general info for each country.'''
 
-    def __init__(self, name, natural_resources, chief_of_state,
+    def __init__(self, name, chief_of_state,
                  head_of_government, government_type, capital,
                  legislature, judiciary, ambassador_to_us,
                  us_ambassador, area_total, area_land, area_water,
@@ -18,7 +18,6 @@ class Country:
                  literacy_last_update, last_update_of_pdf):
 
         self.name = name
-        self.natural_resources = natural_resources
         self.chief_of_state = chief_of_state
         self.head_of_government = head_of_government
         self.government_type = government_type
@@ -52,7 +51,13 @@ class Country:
         self.last_update_of_pdf = last_update_of_pdf
 
 
-class Language:
+class CountryNaturalResources:
+    def __init__(self, country, resource):
+        self.country = country
+        self.resource = resource
+
+
+class CountryLanguage:
     def __init__(self, country_name, language, percent, officiality, year):
         self.country_name = country_name
         self.language = language
@@ -61,7 +66,7 @@ class Language:
         self.year = year
 
 
-class Religion:
+class CountryReligion:
     def __init__(self, country_name, religion, percent, year):
         self.country_name = country_name
         self.religion = religion
@@ -69,7 +74,7 @@ class Religion:
         self.year = year
 
 
-class Ethnicity:
+class CountryEthnicity:
     def __init__(self, country_name, ethnicity, percent, year):
         self.country_name = country_name
         self.ethnicity = ethnicity
@@ -77,16 +82,18 @@ class Ethnicity:
         self.year = year
 
 
-class ImportPartners:
-    def __init__(self, country_name, import_partner, import_percent, import_year):
+class CountryImportPartners:
+    def __init__(self, country_name, import_partner, import_percent,
+                 import_year):
         self.country_name = country_name
         self.import_partner = import_partner
         self.import_percent = import_percent
         self.import_year = import_year
 
 
-class ExportPartners:
-    def __init__(self, country_name, import_partner, import_percent, import_year):
+class CountryExportPartners:
+    def __init__(self, country_name, export_partner, export_percent,
+                 export_year):
         self.country_name = country_name
         self.export_partner = export_partner
         self.export_percent = export_percent
