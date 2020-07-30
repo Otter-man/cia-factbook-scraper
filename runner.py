0a96b40342b2f729a5dc8a8c1be276bf02041554
Otter-man <tereschenko.danila@gmail.com>
2020-07-30 19:42:37 +0700

    fixed name of imports

--- runner.py
+++ runner.py
@@ -1,17 +1,18 @@
 """Main script to run all other modules in project."""
 import os
 import scripts.sqlite_module as sm
-from scripts.pdf_downloader_module import pdf_link_scraper, download_pdf
-from scripts.pdf_scraper_module import pdf_scraper
+from scripts.pdf_downloader_module import scrape_pdf_links, download_pdf
+from scripts.pdf_scraper_module import scrape_pdf
 
+CIA_PAGE = """https://www.cia.gov/library/publications/resources/the-world-factbook/docs/one_page_summaries.html"""
 
-links = pdf_link_scraper()  # make dictionary of links for PDF
+links = scrape_pdf_links(CIA_PAGE)  # make dictionary of links for PDF
 print("Downloading PDFs...")
 
 download_pdf(links)  # download PDF to folder "pdf"
 print("Starting scraping PDFs for text...")
 
-data_containers = pdf_scraper('pdf')
+data_containers = scrape_pdf('pdf')
 print("Finished preparing objects")
 
 tables = [
