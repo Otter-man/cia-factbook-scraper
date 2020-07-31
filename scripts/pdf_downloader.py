0baa600bbe0bad06f7e2e38fd15ec45e3ece3f8a
Otter-man <tereschenko.danila@gmail.com>
2020-07-31 18:13:44 +0700

    added function for async download of PDF

--- scripts/pdf_downloader.py
+++ scripts/pdf_downloader.py
@@ -1,6 +1,6 @@
 """Module for scraping CIA page for links and downloading PDF files."""
 import os
-from requests_html import HTMLSession
+from requests_html import HTMLSession, AsyncHTMLSession
 
 
 def scrape_pdf_links(cia_page):
@@ -10,25 +10,33 @@ def scrape_pdf_links(cia_page):
         cia_page (str): link to the page, containing PDF files.
 
     Returns:
-        dict: country names are used as keys, link to corresponding PDF
-        file as values.
+        List of lists. Each nested list contains 5 elements, each element
+        is a list made of two str - country name and link to PDF file.
     """
 
-    links = {}
+    link_blocks = []
+    links_temp = []
 
     session = HTMLSession()
     source = session.get(cia_page).html  # downloading page as html
 
     country_data = source.find("div.country-name")
 
+    count = 0
     for country in country_data:
-        links[country.text] = list(country.absolute_links)[0]
+        links_temp.append([country.text, list(country.absolute_links)[0]])
+        count += 1
+        if count % 5 == 0:
+            link_blocks.append(links_temp)
+            links_temp = []
+        elif count % 5 != 0 and count == len(list(country_data)):
+            link_blocks.append(links_temp)
 
-    return links
+    return link_blocks
 
 
-def download_pdf(country, link, path_to_folder):
-    """Take dict of links and download them in folder 'PDF'.
+def download_pdf_single(country, link, path_to_folder):
+    """Take country and link and download single PDF.
 
     Args:
         country (str): name of country.
@@ -43,3 +51,55 @@ def download_pdf(country, link, path_to_folder):
     with open(file_path, "wb") as pdf_file:
         pdf_as_page = HTMLSession().get(link)
         pdf_file.write(pdf_as_page.content)
+
+
+def download_pdf_multi(block, path_to_folder):
+    """Take list of links and download them five files at a time.
+
+    Args:
+        block (list): contains 5 elements, each element is a list made of
+            two str - country name and link to corresponding PDF file.
+        path_to_folder (str): path to folder for saving PDF.
+    """
+    async_ses = AsyncHTMLSession()
+
+    link1 = block[0][1]
+    link2 = block[1][1]
+    link3 = block[2][1]
+    link4 = block[3][1]
+    link5 = block[4][1]
+
+    async def downloader1():
+        pdf_as_page = await async_ses.get(link1)
+        return pdf_as_page
+
+    async def downloader2():
+        pdf_as_page = await async_ses.get(link2)
+        return pdf_as_page
+
+    async def downloader3():
+        pdf_as_page = await async_ses.get(link3)
+        return pdf_as_page
+
+    async def downloader4():
+        pdf_as_page = await async_ses.get(link4)
+        return pdf_as_page
+
+    async def downloader5():
+        pdf_as_page = await async_ses.get(link5)
+        return pdf_as_page
+
+    pages_return = async_ses.run(
+        downloader1, downloader2, downloader3, downloader4, downloader5)
+
+    counter = 0
+    for page in pages_return:
+
+        file_name = [i[0] for i in block if page.url == i[1]]
+        file_name = file_name[0] + ".pdf"
+
+        file_path = os.path.join(path_to_folder, file_name)
+
+        with open(file_path, "wb") as pdf_file:
+            pdf_file.write(page.content)
+        counter += 1
