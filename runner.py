25ce6dd55c7c253c5f548fc543b7069b06f6af33
Otter-man <tereschenko.danila@gmail.com>
2020-07-31 18:14:34 +0700

    modified script to use async download

--- runner.py
+++ runner.py
@@ -1,12 +1,12 @@
 """Main script to run all other modules in project."""
 import os
 import scripts.sqlite as sq
-from scripts.pdf_downloader import scrape_pdf_links, download_pdf
+import scripts.pdf_downloader as pd
 from scripts.pdf_scraper import scrape_pdf
 
 CIA_PAGE = """https://www.cia.gov/library/publications/resources/the-world-factbook/docs/one_page_summaries.html"""
 
-links = scrape_pdf_links(CIA_PAGE)  # make dictionary of links for PDF
+links = pd.scrape_pdf_links(CIA_PAGE)  # make list with links for PDF
 print("Downloading PDFs...")
 
 PDF_FOLDER_PATH = "pdf"
@@ -14,13 +14,19 @@ PDF_FOLDER_PATH = "pdf"
 if not os.path.exists(PDF_FOLDER_PATH):
     os.mkdir(PDF_FOLDER_PATH)
 
+for block in links:
+    try:
+        # download PDF 5 at a time
+        pd.download_pdf_multi(block, PDF_FOLDER_PATH)
+    except IndexError:
+        for item in block:
+            # download PDF from last block one by one
+            pd.download_pdf_single(item[0], item[1], PDF_FOLDER_PATH)
 
-for country, link in links.items():  # download PDFs to folder "pdf"
-    download_pdf(country, link, PDF_FOLDER_PATH)
 print("Finished downloading all PDFs")
 
-print("Starting scraping PDFs for text...")
 
+print("Starting scraping PDFs for text...")
 data_containers = scrape_pdf(PDF_FOLDER_PATH)  # scrape data to lists
 print("Finished preparing objects")
 
